39666b4603af8e1eda5eb5fcc565a7add0826979
airtago <airtago@gmail.com>
2017-03-19 17:16:18 +0300

    [+] using only paired packets.

--- Application/src/main/java/com/example/android/bluetoothlegatt/DetectParams.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DetectParams.java
@@ -3,7 +3,7 @@ package com.example.android.bluetoothlegatt;
 import android.content.SharedPreferences;
 
 class DetectParams {
-    static int    AVG_CNT = 5;
+    static long   AVG_TIME = 1000;
     static double DIFF_THRESHOLD = 10.0;
     static boolean DEV_MODE = false;
     static boolean INVERT_ANTS_IDXS = false;
@@ -12,6 +12,6 @@ class DetectParams {
         DEV_MODE = sp.getBoolean("DEV_MODE", false);
         INVERT_ANTS_IDXS = sp.getBoolean("INVERT_ANTS_IDXS", false);
         DIFF_THRESHOLD = Double.parseDouble(sp.getString("DIFF_THRESHOLD", Double.toString(DIFF_THRESHOLD)));
-        AVG_CNT = Integer.parseInt(sp.getString("AVG_CNT", Integer.toString(AVG_CNT)));
+        AVG_TIME = Long.parseLong(sp.getString("AVG_TIME", Long.toString(AVG_TIME)));
     }
 }
